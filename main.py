ed221eb763a27a78b9f881871bbbef02c4da072c
DeFchanel <georgyasr@mail.ru>
2023-09-20 19:44:15 +0300

    download books from tululu.org

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ import os
 
 dir_name = 'books'
 os.makedirs(dir_name, exist_ok=True)
-                
+
 for i  in range(1, 11):
     url = f"https://tululu.org/txt.php?id={i}"
-
     response = requests.get(url)
     response.raise_for_status() 
-
     filename = f'books/id{i}.txt'
     with open(filename, 'wb') as file:
         file.write(response.content)
