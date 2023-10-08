b0b9a77125ac347db9345a0b581291917dbb1619
DeFchanel <georgyasr@mail.ru>
2023-10-08 22:20:51 +0300

    download book covers

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import requests
 import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlsplit
 
 
-dir_name = 'books'
-os.makedirs(dir_name, exist_ok=True)
+dir1_name = 'books'
+dir2_name = 'images'
+os.makedirs(dir1_name, exist_ok=True)
+os.makedirs(dir2_name, exist_ok=True)
+
+
+def get_soup(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    return soup
+
 
 def check_for_redirect(response):
     if response.url == 'https://tululu.org/':
@@ -17,8 +28,9 @@ def download_books():
         response = requests.get(url)
         response.raise_for_status()
         try:
-            book_url = f"https://tululu.org/b{i}/"
             check_for_redirect(response)
+            book_url = f"https://tululu.org/b{i}/"
+            print(download_img(book_url))
             title = get_title(book_url)
             download_txt(url, f'{i}.{title}', folder='books/')
         except requests.HTTPError:
@@ -32,13 +44,26 @@ def download_txt(url, filename, folder='books/'):
     with open(filepath, 'wb') as file:
         file.write(response.content)
 
-def get_title(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
+def get_title(book_url):
+    soup = get_soup(book_url)
     title_tag = soup.find('h1')
     title_text = title_tag.text
     title = title_text.split('::')[0].strip()
     return title
+
+
+def download_img(book_url, folder='images/'):
+    soup = get_soup(book_url)
+    site_url = 'https://tululu.org/'
+    img_tag = soup.find('div', class_='bookimage').find('img')['src']
+    img_url = urljoin(site_url, img_tag)
+    response = requests.get(img_url)
+    response.raise_for_status()
+    filename = urlsplit(img_tag).path
+    filename1 = filename.split('/')[-1]
+    filepath = os.path.join(folder, filename1)
+    print(filepath)
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
 
 download_books()
