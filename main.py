87257bfbdfcbb13515fa76238d57502451ad0758
DeFchanel <georgyasr@mail.ru>
2023-10-16 19:53:08 +0300

    added parse_book_page

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ os.makedirs(dir1_name, exist_ok=True)
 os.makedirs(dir2_name, exist_ok=True)
 
 
+def check_for_redirect(response):
+    if response.url == 'https://tululu.org/':
+        raise requests.HTTPError
+    
+
 def get_soup(url):
     response = requests.get(url)
     response.raise_for_status()
@@ -18,28 +23,24 @@ def get_soup(url):
     return soup
 
 
-def check_for_redirect(response):
-    if response.url == 'https://tululu.org/':
-        raise requests.HTTPError
+def get_book_genres(soup):
+    genres = soup.find_all(class_='d_book')[1]
+    genres1 = genres.find_all('a')
+    return [x.text for x in genres1]
 
-def download_books():
-    for i in range(1, 11):
-        url = f"https://tululu.org/txt.php?id={i}"
-        response = requests.get(url)
-        response.raise_for_status()
-        try:
 
-            check_for_redirect(response)
-            book_url = f"https://tululu.org/b{i}/"
-            soup = get_soup(book_url)
-            download_img(soup)
-            title = get_title(soup)
-            download_txt(url, f'{i}.{title}', folder='books/')
-            print(title)
-            get_book_genre(soup)
-            #download_comments(soup)
-        except requests.HTTPError:
-            continue
+def get_title_and_author(soup):
+    title_tag = soup.find('h1')
+    title_text = title_tag.text
+    title = title_text.split('::')[0].strip()
+    author = title_text.split('::')[1].strip()
+    return [title, author]
+
+
+def get_comments(soup):
+    comments = soup.find_all(class_='texts')
+    return [comment.find(class_='black').text for comment in comments]
+
 
 def download_txt(url, filename, folder='books/'):
     response = requests.get(url)
@@ -48,26 +49,6 @@ def download_txt(url, filename, folder='books/'):
     filepath = os.path.join(folder, sanitized_filename)
     with open(filepath, 'wb') as file:
         file.write(response.content)
-
-def get_title(soup):
-    title_tag = soup.find('h1')
-    title_text = title_tag.text
-    title = title_text.split('::')[0].strip()
-    return title
-
-
-def get_book_genre(soup):
-    genres = soup.find_all(class_='d_book')[1]
-    genres1 = genres.find_all('a')
-    print([x.text for x in genres1])
-    
-
-
-def download_comments(soup):
-    comments = soup.find_all(class_='texts')
-    for comment in comments:
-        if comment:
-            print(comment.find(class_='black').text)
 
 
 def download_img(soup, folder='images/'):
@@ -81,5 +62,37 @@ def download_img(soup, folder='images/'):
     filepath = os.path.join(folder, filename1)
     with open(filepath, 'wb') as file:
         file.write(response.content)
+
+
+def parse_book_page(soup):
+    title = get_title_and_author(soup)[0]
+    author = get_title_and_author(soup)[1]
+    genres = get_book_genres(soup)
+    comments = get_comments(soup)
+    return {
+        'title': title,
+        'author': author,
+        'genres': genres,
+        'comments': comments
+    }
+
+
+def download_books():
+    for i in range(1, 11):
+        url = f"https://tululu.org/txt.php?id={i}"
+        response = requests.get(url)
+        response.raise_for_status()
+        try:
+            check_for_redirect(response)
+            book_url = f"https://tululu.org/b{i}/"
+            soup = get_soup(book_url)
+            parsed_page = parse_book_page(soup)
+            download_img(soup)
+            title = parsed_page['title']
+            download_txt(url, f'{i}.{title}', folder='books/')
+            print(parsed_page)
+        except requests.HTTPError:
+            continue
+
 
 download_books()
